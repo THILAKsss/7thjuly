af72119a4dc74ee30b99de8f69051682090547bb
admin <admin@DESKTOP-JC379HT>
2024-07-07 11:47:32 +0530

    fisrt

---
+++ src/first/FirstProj.java
@@ -0,0 +1,18 @@
+package first;
+
+public class FirstProj {
+
+	public static void  main(String[] args) {
+		
+		int a =(int)4.5f; // exp typecast
+		short s =(short) 7.7;
+		System.out.println(a);
+		System.out.println(s);
+		
+		double d =8.90f; //imp type
+		
+		
+		
+	}
+	
+}
---
+++ src/first/Second.java
@@ -0,0 +1,14 @@
+package first;
+
+public class Second {
+	
+	public static void main(String[] args) {
+
+	System.out.println("HEllo");
+	
+	int a =10; 
+	long b =20;
+	
+	float ab= 4.5F;
+}
+}
